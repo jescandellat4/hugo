3397739b6a42d306b90238933b8a113e1f3486b6
Joan Josep Escandell <jescandell@at4.net>
2018-11-14 10:51:28 +0100

    home page amp

--- build.ps1
+++ build.ps1
@@ -1,9 +1,12 @@
 # delete previous content
 Remove-Item C:\inetpub\wwwroot\at4-hugo\* -Recurse -Force
 
-# compile less
+# compile less for inline amp
 lessc themes/at4Theme/less/_style.less themes/at4Theme/layouts/partials/styles.css
+
+# compile less for html
 lessc themes/at4Theme/less/_style.less themes/at4Theme/static/css/styles.css
 
-# generate html
-hugo --minify 
+# generate static content and minify
+# hugo --minify 
+hugo
--- content/blog/_index.md
+++ content/blog/_index.md
@@ -3,9 +3,9 @@ title: "Blog"
 date: 2018-11-08T16:10:44+01:00
 menu:
     header_menu:
-        name: "Blog"
+        name: "Notícias"
     sidebar_menu:
-        name: "Blog"
+        name: "Notícias"
     footer_menu:
-        name: "Blog"
+        name: "Notícias"
 ---
